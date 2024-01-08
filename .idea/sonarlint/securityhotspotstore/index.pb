83411bc276282c1261b12112a434466eb72335fb
Bartłomiej Wojcik <github.bartek.wojcik@gmail.com>
2024-01-08 21:41:49 +0100

    API

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+f
+6app/src/main/java/pl/edu/pb/filmoteka/ApiResponse.java,d\3\d3fb49527e040fdd1d2ce0b2ef9e7719ad6ab44d
+g
+7app/src/main/java/pl/edu/pb/filmoteka/MainActivity.java,2\2\22aa19447b9b280b3800645d772739cbe4a1abb5
--- app/src/main/java/pl/edu/pb/filmoteka/MainActivity.java
+++ app/src/main/java/pl/edu/pb/filmoteka/MainActivity.java
@@ -1,8 +1,13 @@
 package pl.edu.pb.filmoteka;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new MyAsyncTask().execute();
+    }
+
+    private class MyAsyncTask extends AsyncTask<Void, Void, ApiResponse> {
+
+        @Override
+        protected ApiResponse doInBackground(Void... params) {
+            try {
+                OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder()
+                        .url("https://api.themoviedb.org/3/account/20889746/favorite/movies?language=en-US&page=1&sort_by=created_at.asc")
+                        .get()
+                        .addHeader("accept", "application/json")
+                        .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmI1OTA2OTU4ZDY0YjRmOWM1MjMzMzQxNjM3M2Y0YiIsInN1YiI6IjY1OTVhYTFjNTkwN2RlMDE2NzYzYmYwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IlVmj8Oxv5RunQqXK55LVmJerMote8EMPNsO6jcEdRA")
+                        .build();
+
+                Response response = client.newCall(request).execute();
+                String responseBody = response.body().string();
+
+                // Sprawdź, czy odpowiedź zawiera oczekiwane dane
+                if (response.isSuccessful()) {
+                    return new ApiResponse(true, responseBody);
+                } else {
+                    return new ApiResponse(false, null, "HTTP Error: " + response.code());
+                }
+            } catch (Exception e) {
+                Log.e("MainActivity", "Error during API call", e);
+                return new ApiResponse(false, null, "Error: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ApiResponse result) {
+            // Tutaj możesz obsłużyć wynik zapytania do API
+            if (result != null) {
+                if (result.isSuccess()) {
+                    Log.d("MainActivity", "Response: " + result.getResponseData());
+                    // Dodaj kod obsługi połączenia udanego
+                } else {
+                    Log.e("MainActivity", "Error: " + result.getErrorMessage());
+                    // Dodaj kod obsługi błędu połączenia
+                }
+            }
+        }
     }
 }
